7b04558aab13ca757253bd7053b7eb364beff384
Luca Dall'Olio <luca.dallolio@aego.ai>
2023-06-11 15:05:33 +0200

    adding includedir to configure but it seems to be ignored

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -7,7 +7,7 @@ rem if errorlevel 1 exit 1
 
 set MAKE=mingw32-make
 
-bash -c "alias make=mingw32-make && ./configure --prefix=`cygpath -u $PREFIX` --bindir=`cygpath -u $LIBRARY_BIN` --libdir=`cygpath -u $LIBRARY_LIB` PYTHON=`which python` --enable-shared --disable-dependency-tracking --disable-python" # --enable-octave
+bash -c "alias make=mingw32-make && ./configure --prefix=`cygpath -u $PREFIX` --bindir=`cygpath -u $LIBRARY_BIN` --libdir=`cygpath -u $LIBRARY_LIB` includedir=`cygpath -u $LIBRARY_INC` PYTHON=`which python` --enable-shared --disable-dependency-tracking --disable-python" # --enable-octave
 if errorlevel 1 exit 1
 
 mingw32-make -j %CPU_COUNT%
